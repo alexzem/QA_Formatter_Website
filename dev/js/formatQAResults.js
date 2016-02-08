da3150355cbd0abc1237b2ee6b399354af5ab8e9
Alex Zem <azemelshteyn@gmail.com>
2016-02-08 17:13:46 -0500

    Format additional flags
    Formats additional flags with defined strings to replace or remove
    feedback. Also adds only the related categories to the feedback.

--- dev/js/formatQAResults.js
+++ dev/js/formatQAResults.js
@@ -3,222 +3,259 @@ var qaOutput;
 var defaultOutput;
 var lastInput = "";
 var checkForInputIntervalID;
+var formatAdditionalCb;
 
 window.addEventListener("load", init);
 
 function init() {
-	setGlobalVariables();
-	addEventListeners();
+    setGlobalVariables();
+    addEventListeners();
 }
 
 function setGlobalVariables() {
-	qaInput = document.getElementById("qaInput");
-	qaOutput = document.getElementById("qaOutput");
-	defaultOutput = qaOutput.innerHTML;
+    qaInput = document.getElementById("qaInput");
+    qaOutput = document.getElementById("qaOutput");
+    defaultOutput = qaOutput.innerHTML;
+    formatAdditionalCb = document.getElementById("formatAdditionalCb");
 }
 
 function addEventListeners() {
-	var clearButton = document.getElementById("clearButton");
-	var selectButton = document.getElementById("selectButton");
+    var clearButton = document.getElementById("clearButton");
+    var selectButton = document.getElementById("selectButton");
 
-	clearButton.addEventListener("click", resetQAInputText);
-	selectButton.addEventListener("click", selectOutputText);
+    clearButton.addEventListener("click", resetQAInputText);
+    selectButton.addEventListener("click", selectOutputText);
 
-	qaInput.addEventListener("keyup", updateOutput);
-	qaInput.addEventListener("blur", updateOutput);
-	qaInput.addEventListener("paste", function() {
-		setTimeout(updateOutput, 100);
-	});
+    qaInput.addEventListener("keyup", updateOutput);
+    qaInput.addEventListener("blur", updateOutput);
+    qaInput.addEventListener("paste", function () {
+        setTimeout(updateOutput, 100);
+    });
+
+    formatAdditionalCb.addEventListener("change", updateOutput);
 }
 
 function resetQAInputText(event) {
-	setInput("");
-	setOutput(defaultOutput);
+    setInput("");
+    setOutput(defaultOutput);
 }
 
 function checkForNewInput() {
-	if (lastInput !== getInput()) {
-		lastInput = getInput();
-		updateOutput();
-	}
+    if (lastInput !== getInput()) {
+        lastInput = getInput();
+        updateOutput();
+    }
 }
 
+//trim string to make sure output displays if pasted with whitespace
 function getInput() {
-	return qaInput.value;
+    return qaInput.value.trim();
 }
 
 function setInput(input) {
-	qaInput.value = input;
+    qaInput.value = input.trim();
 }
 
 function getOutput() {
-	var output = unescape(qaOutput.innerHTML);
-	return output;
+    var output = unescape(qaOutput.innerHTML);
+    return output;
 }
 
 function setOutput(output) {
-	qaOutput.innerHTML = output;
+    qaOutput.innerHTML = output;
 }
 
-function isInt(value){ 
-	var intRegex = /^\d+$/;
-	if(intRegex.test(value)) {
-		return true;
-	}
-	else {
-		return false;
-	}
+function isInt(value) {
+    var intRegex = /^\d+$/;
+    if (intRegex.test(value)) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 function updateOutput() {
-	if (!isStringBlank(getInput())) {
-		var output;
 
-		var adFeedbackCollection = new AdFeedbackCollection(getInput());
-		printOutput(adFeedbackCollection);
-	}
-	else {
-		qaOutput.innerHTML = defaultOutput;
-	}
+    if (!isStringBlank(getInput())) {
+        var output = getInput();
+
+        if (formatAdditionalCb.checked) {
+            //Remove
+            for (var i = 0; i < formatAdditionalResultsConfig.remove.length; i++) {
+                output = output.replace(new RegExp(formatAdditionalResultsConfig.remove[i], 'ig'), '');
+            }
+
+            //Replace
+            for (var j = 0; j < formatAdditionalResultsConfig.replace.length; j++) {
+                output = output.replace(new RegExp(formatAdditionalResultsConfig.replace[j][0], 'ig'), formatAdditionalResultsConfig.replace[j][1]);
+            }
+        }
+
+        if (isStringBlank(output)) {
+            qaOutput.innerHTML = 'Ad(s) passed QA. No issues present.';
+            return;
+        }
+
+        var adFeedbackCollection = new AdFeedbackCollection(output.trim());
+        printOutput(adFeedbackCollection);
+
+    } else {
+        qaOutput.innerHTML = defaultOutput;
+    }
+
 }
 
 function isStringBlank(string) {
-	var whiteSpace = /^\s+$/;
+    var whiteSpace = /^\s+$/;
 
-	return whiteSpace.test(string) || string.length === 0 || string === "";
+    return whiteSpace.test(string) || string.length === 0 || string === "";
 }
 
 function printOutput(adFeedbackCollection) {
-	var qaResults = adFeedbackCollection.qaResults;
-	var outputString = "";
-	
-	if (qaResults.length > 0) {
-		outputString = "<p><span class=siteSpecification>Site Specification</span><br/>";
-		outputString += "<span class=functionality>Functionality</span><br/>";
-		outputString += "<span class=tracking>Tracking</span></p><br/>";
-	}
-	
-	outputString += qaResults.map(getAdFeedback).join("<br/>");
-	outputString = outputString.replace(/[\u201C\u201D\u201E\u201F\u2033\u2036]/g, "\"");
-	
-	qaOutput.innerHTML = outputString;
+
+    var qaResults = adFeedbackCollection.qaResults;
+    var outputString = "";
+
+    if (qaResults.length > 0) {
+
+        outputString += '<p>';
+        if (adFeedbackCollection.qaCategories.indexOf('Site Specification') >= 0) {
+            outputString += "<span class=siteSpecification>Site Specification</span><br/>";
+        }
+
+        if (adFeedbackCollection.qaCategories.indexOf('Functionality') >= 0) {
+            outputString += "<span class=functionality>Functionality</span><br/>";
+        }
+
+        if (adFeedbackCollection.qaCategories.indexOf('Tracking') >= 0) {
+            outputString += "<span class=tracking>Tracking</span>";
+        }
+        outputString += '</p><br/>';
+
+    }
+
+    outputString += qaResults.map(getAdFeedback).join("<br/>");
+    outputString = outputString.replace(/[\u201C\u201D\u201E\u201F\u2033\u2036]/g, "\"");
+
+    qaOutput.innerHTML = outputString;
 }
 
 function getAdFeedback(adFeedback) {
-	//Ad ID and title
-	var html = adFeedback.ads.map(getAdTitles).join("");
+    //Ad ID and title
+    var html = adFeedback.ads.map(getAdTitles).join("");
 
-	//Ad issues
-	html += "<ul>";
-	
-	if (adFeedback.issues.length > 0) {
-		html += adFeedback.issues.map(getIssues, "").join("");
-	}
-	else {
-		html += "<li>See \"All Ads\"</li>";
-	}
-	
-	html += "</ul>";
+    //Ad issues
+    html += "<ul>";
 
-	return html;
+    if (adFeedback.issues.length > 0) {
+        html += adFeedback.issues.map(getIssues, "").join("");
+    } else {
+        html += "<li>See \"All Ads\"</li>";
+    }
+
+    html += "</ul>";
+
+    return html;
 }
 
 function getAdTitles(ad) {
-	var html = "<p class='bold'>";
-	html += ad.id ? ad.id + " - " : "";
-	html += ad.title;
-	html += ad.format ? " (" + ad.format + ")": "";
-	html += "</p>";
+    var html = "<p class='bold'>";
+    html += ad.id ? ad.id + " - " : "";
+    html += ad.title;
+    html += ad.format ? " (" + ad.format + ")" : "";
+    html += "</p>";
 
-        if (ad.testPages && ad.testPages.length > 0) {
-            html += "<p>Test page(s): ";
+    if (ad.testPages && ad.testPages.length > 0) {
+        html += "<p>Test page(s): ";
 
-            var anchors = ad.testPages.map(function (testPage) {
-                return "<a href='" + testPage + "' target='_blank'>" + testPage + "</a>";
-            });
-            
-            html += anchors.join(", ");
-            html += "</p>";
-        }
-	
-	return html;
+        var anchors = ad.testPages.map(function (testPage) {
+            return "<a href='" + testPage + "' target='_blank'>" + testPage + "</a>";
+        });
+
+        html += anchors.join(", ");
+        html += "</p>";
+    }
+
+    return html;
 }
 
+
+
 function getIssues(issue) {
-	var categoryClass = getCategoryClass(issue.category);
-	var html = "<li class=";
+    var categoryClass = getCategoryClass(issue.category);
+    var html = "<li class=";
 
-	html += categoryClass;
-	html += ">";
+    html += categoryClass;
+    html += ">";
 
-	if (issue.problemsIssues.length > 1) {
-		html += issue.problemsIssues;
+    if (issue.problemsIssues.length > 1) {
+        html += issue.problemsIssues;
 
-		var isNotStandardCategory = categoryClass.length === 0;
+        var isNotStandardCategory = categoryClass.length === 0;
 
-		if (isNotStandardCategory && issue.adElements.length > 0) {
-			html += " [" + issue.adElements.join(', ') + "]";
-		}
+        if (isNotStandardCategory && issue.adElements.length > 0) {
+            html += " [" + issue.adElements.join(', ') + "]";
+        }
 
-	}
+    }
 
-	if (issue.additionalNotes) {
-		if(issue.additionalNotes.length > 1) {
-			if (issue.additionalNotes.length <= 10) {
-				html += " (" + issue.additionalNotes + ")";
-			}
-			else {
-				if (issue.problemsIssues.length > 1) {
-					html += " - ";
-				}
-				html += issue.additionalNotes;
-			}
-		}
-	}
+    if (issue.additionalNotes) {
+        if (issue.additionalNotes.length > 1) {
+            if (issue.additionalNotes.length <= 10) {
+                html += " (" + issue.additionalNotes + ")";
+            } else {
+                if (issue.problemsIssues.length > 1) {
+                    html += " - ";
+                }
+                html += issue.additionalNotes;
+            }
+        }
+    }
 
-	if (issue.testPages && issue.testPages.length > 0) {
-		html += "<br/>";
-		html += "Test page(s): " + issue.testPages.join(" | ");
-	}
+    if (issue.testPages && issue.testPages.length > 0) {
+        html += "<br/>";
+        html += "Test page(s): " + issue.testPages.join(" | ");
+    }
 
-	html += "</li>";
+    html += "</li>";
 
-	return html;
+    return html;
 }
 
 function getCategoryClass(category) {
-	switch(category) {
-		case "Site Specification":
-			return "siteSpecification";
-		case "Functionality":
-			return "functionality";
-		case "Tracking":
-			return "tracking";
-		default:
-			return "";
-	}
+    switch (category) {
+        case "Site Specification":
+            return "siteSpecification";
+        case "Functionality":
+            return "functionality";
+        case "Tracking":
+            return "tracking";
+        default:
+            return "";
+    }
 }
 
 function selectText(element) {
     var range;
     var selection;
-       
+
     if (document.body.createTextRange) {
         range = document.body.createTextRange();
         range.moveToElementText(element);
         range.select();
     } else if (window.getSelection) {
-        selection = window.getSelection();        
+        selection = window.getSelection();
         range = document.createRange();
         range.selectNodeContents(element);
         selection.removeAllRanges();
         selection.addRange(range);
-    } }
+    }
+}
 
 function ascendingNumericalSort(a, b) {
-	return Number(a) - Number(b);
+    return Number(a) - Number(b);
 }
 
 function selectOutputText() {
-	selectText(qaOutput);
+    selectText(qaOutput);
 }
--- dev/js/qaObjects.js
+++ dev/js/qaObjects.js
@@ -1,311 +1,318 @@
 function AdFeedbackCollection(inputText) {
-	this.qaResults = [];
-	
-	this.parseFeedback(inputText);
-        this.isolateCommonTestPages();
-	this.isolateCommonIssues();
-	this.combineLikeIssues();
+    this.qaResults = [];
+    this.qaCategories = [];
+    this.parseFeedback(inputText);
+    this.isolateCommonTestPages();
+    this.isolateCommonIssues();
+    this.combineLikeIssues();
 }
 
 AdFeedbackCollection.prototype = {
-	parseRawText: function (input) {
-		var splitPattern = /(?:\t)|(?:\n(?=[1-9]\d{6,}))/g;
-		var allCells = input.split(splitPattern);
-		var rows = [];
-		var columns = [];
-		
-		allCells.forEach(function (cell, i) {
-			if (i > 0 && i % 8 === 0) {
-				rows.push(columns);
-				columns = [];
-			}
-			
-			columns.push(cell);
-		});
-		
-		rows.push(columns);
-		
-		return rows;
-	},
+    parseRawText: function (input) {
+        var splitPattern = /(?:\t)|(?:\n(?=[1-9]\d{6,}))/g;
+        var allCells = input.split(splitPattern);
+        var rows = [];
+        var columns = [];
 
-	parseFeedback: function (input) {
-		var ID = 0;
-		var TITLE = 1;
-		var FORMAT = 2;
-		var CATEGORY = 3;
-		var AD_ELEMENT = 4;
-		var PROBLEMS_ISSUES = 5;
-		var TEST_PAGE = 6;
-		var ADDITIONAL_NOTES = 7;
-			
-		var feedbacks = this.parseRawText(input);
-			
-		feedbacks.forEach(function (feedback, i, feedbacks) {
-			var issue = new Issue({
-				category: feedback[CATEGORY],
-				problemsIssues: feedback[PROBLEMS_ISSUES],
-				adElement: feedback[AD_ELEMENT],
-				testPage: feedback[TEST_PAGE],
-				additionalNotes: feedback[ADDITIONAL_NOTES]
-			});
-				
-				
-			if (i === 0 || feedback[ID] != feedbacks[i - 1][ID]) {
-				var ad = new Ad({
-					id: feedback[ID], 
-					title: feedback[TITLE], 
-					format: feedback[FORMAT]
-				});
-					
-				this.qaResults.push(new AdFeedback({
-					ads: [ad],
-					issues: [issue]
-				}));
-			}
-			else {
-				this.qaResults[this.qaResults.length - 1].insertIssue(issue);
-			}
-		}, this);
-	},
-        
-        isolateCommonTestPages: function() {
-            if (this.qaResults.length > 1) {
-                this.qaResults.forEach(function (qaResult) {
-                    var isThereAtLeastOneIssue = qaResult.issues && qaResult.issues.length > 0;
-                    
-                    if (isThereAtLeastOneIssue) {
-                        var firstIssue = qaResult.issues[0];
-                        var isThereAtLeastOneTestPage = firstIssue.testPages && firstIssue.testPages.length > 0; 
+        allCells.forEach(function (cell, i) {
+            if (i > 0 && i % 8 === 0) {
+                rows.push(columns);
+                columns = [];
+            }
 
-                        if (isThereAtLeastOneTestPage) {
-                            var doAllIssuesHaveSameTestPage = qaResult.issues.every(function (issue) {
-                                return issue.testPages.sort().join(',') === firstIssue.testPages.sort().join(',');
+            columns.push(cell);
+        });
+
+        rows.push(columns);
+
+        return rows;
+    },
+
+    parseFeedback: function (input) {
+        var ID = 0;
+        var TITLE = 1;
+        var FORMAT = 2;
+        var CATEGORY = 3;
+        var AD_ELEMENT = 4;
+        var PROBLEMS_ISSUES = 5;
+        var TEST_PAGE = 6;
+        var ADDITIONAL_NOTES = 7;
+
+        var feedbacks = this.parseRawText(input);
+
+        feedbacks.forEach(function (feedback, i, feedbacks) {
+            var issue = new Issue({
+                category: feedback[CATEGORY],
+                problemsIssues: feedback[PROBLEMS_ISSUES],
+                adElement: feedback[AD_ELEMENT],
+                testPage: feedback[TEST_PAGE],
+                additionalNotes: feedback[ADDITIONAL_NOTES]
+            });
+
+            this.qaCategories.addUnique(issue.category);
+
+            if (i === 0 || feedback[ID] != feedbacks[i - 1][ID]) {
+                var ad = new Ad({
+                    id: feedback[ID],
+                    title: feedback[TITLE],
+                    format: feedback[FORMAT]
+                });
+
+                this.qaResults.push(new AdFeedback({
+                    ads: [ad],
+                    issues: [issue]
+                }));
+            }
+            else {
+                this.qaResults[this.qaResults.length - 1].insertIssue(issue);
+            }
+        }, this);
+    },
+
+    isolateCommonTestPages: function () {
+        if (this.qaResults.length > 1) {
+            this.qaResults.forEach(function (qaResult) {
+                var isThereAtLeastOneIssue = qaResult.issues && qaResult.issues.length > 0;
+
+                if (isThereAtLeastOneIssue) {
+                    var firstIssue = qaResult.issues[0];
+                    var isThereAtLeastOneTestPage = firstIssue.testPages && firstIssue.testPages.length > 0;
+
+                    if (isThereAtLeastOneTestPage) {
+                        var doAllIssuesHaveSameTestPage = qaResult.issues.every(function (issue) {
+                            return issue.testPages.sort().join(',') === firstIssue.testPages.sort().join(',');
+                        });
+
+                        if (doAllIssuesHaveSameTestPage) {
+                            qaResult.ads[0].testPages = firstIssue.testPages;
+                            qaResult.issues.forEach(function (issue) {
+                                issue.testPages = null;
                             });
-                            
-                            if (doAllIssuesHaveSameTestPage) {
-                                qaResult.ads[0].testPages = firstIssue.testPages;
-                                qaResult.issues.forEach(function (issue) {
-                                    issue.testPages = null;
-                                });
-                            }
                         }
                     }
+                }
+            });
+        }
+    },
+
+    isolateCommonIssues: function () {
+        if (this.qaResults.length > 1) {
+            var issues = this.qaResults[0].issues;
+
+            var commonIssues = issues.filter(function (issue) {
+                return this.qaResults.every(function (qaResult) {
+                    return qaResult.hasSimilarIssue(issue);
                 });
+            }, this);
+
+            commonIssues.forEach(function (issue) {
+                this.qaResults.forEach(function (qaResult) {
+                    qaResult.removeSimilarIssue(issue);
+                });
+            }, this);
+
+            if (commonIssues.length > 0) {
+                var allAds = new Ad({ id: "", title: "All Ads", format: "" });
+                var commonFeedback = new AdFeedback({
+                    ads: [allAds],
+                    issues: commonIssues
+                });
+
+                this.qaResults.unshift(commonFeedback);
             }
-        },
-		
-	isolateCommonIssues: function () {
-		if (this.qaResults.length > 1) {
-			var issues = this.qaResults[0].issues;
+        }
+    },
 
-			var commonIssues = issues.filter(function (issue) {
-				return this.qaResults.every(function (qaResult) {
-					return qaResult.hasSimilarIssue(issue); 
-				});
-			}, this);
-			
-			commonIssues.forEach(function (issue) {
-				this.qaResults.forEach(function (qaResult) {
-					qaResult.removeSimilarIssue(issue);
-				});
-			}, this);
-
-			if (commonIssues.length > 0) {
-				var allAds = new Ad({id:"", title:"All Ads", format:""});
-				var commonFeedback = new AdFeedback({
-                                    ads: [allAds],
-                                    issues: commonIssues
-                                });
-
-				this.qaResults.unshift(commonFeedback);
-			}
-		}
-	},
-		
-	combineLikeIssues: function () {
-                this.qaResults.forEach(function (qaResult, i, qaResults) {
-			for (var j = i + 1; j < qaResults.length; j++) {
-				if (qaResult.hasSimilarIssuesAs(qaResults[j])) {
-					qaResult.insertAd(qaResults[j].ads[0]);
-					qaResults.splice(j, 1);
-					j--;
-				}
-			}
-		});
-	}
+    combineLikeIssues: function () {
+        this.qaResults.forEach(function (qaResult, i, qaResults) {
+            for (var j = i + 1; j < qaResults.length; j++) {
+                if (qaResult.hasSimilarIssuesAs(qaResults[j])) {
+                    qaResult.insertAd(qaResults[j].ads[0]);
+                    qaResults.splice(j, 1);
+                    j--;
+                }
+            }
+        });
+    }
 };
 
 function AdFeedback(args) {
-	this.ads = args.ads;
-	this.issues = args.issues;
+    this.ads = args.ads;
+    this.issues = args.issues;
 }
 
 AdFeedback.prototype = {
-	insertAd: function (ad) {
-		if (!this.hasAd(ad)) {
-			this.ads.push(ad);
-		}
-	},
-		
-	getAdIndex: function (ad) {
-		for (var i = 0; i < this.ads.length; i++) {
-			if (this.ads[i].isEqualTo(ad)) {
-				return i;
-			}
-		}
-		
-		return -1;
-	},
-		
-	hasAd: function (ad) {
-		if (this.getAdIndex(ad) >= 0) {
-			return true;
-		}
-		
-		return false;
-	},
-		
-	insertIssue: function (issue) {
-		if (!this.hasIssue(issue)) {
-			if (this.hasSimilarIssue(issue)) {
-				this.getSimilarIssue(issue).combine(issue);
-			}
-			else {
-				this.issues.push(issue);
-			}
-		}
-	},
-		
-	getIssueIndex: function (issue) {
-		for (var i = 0; i < this.issues.length; i++) {
-			if (this.issues[i].isEqualTo(issue)) {
-				return i;
-			}
-		}		
-		return -1;
-	},
-		
-	hasIssue: function (issue) {
-		if (this.getIssueIndex(issue) >= 0) {
-			return true;
-		}
-		
-		return false;
-	},
-	
-	getSimilarIssueIndex: function (issue) {
-		for (var i = 0; i < this.issues.length; i++) {
-			if (this.issues[i].isSimilarTo(issue)) {
-				return i;
-			}
-		}
-		return -1;
-	},
+    insertAd: function (ad) {
+        if (!this.hasAd(ad)) {
+            this.ads.push(ad);
+        }
+    },
 
-	getSimilarIssue: function (issue) {
-		var i = this.getSimilarIssueIndex(issue);
-		
-		if (i >= 0) {
-			return this.issues[i];
-		}
+    getAdIndex: function (ad) {
+        for (var i = 0; i < this.ads.length; i++) {
+            if (this.ads[i].isEqualTo(ad)) {
+                return i;
+            }
+        }
 
-		return undefined;
-	},
+        return -1;
+    },
 
-	hasSimilarIssue: function (issue) {
-		if (this.getSimilarIssueIndex(issue) >= 0) {
-			return true;
-		}
+    hasAd: function (ad) {
+        if (this.getAdIndex(ad) >= 0) {
+            return true;
+        }
 
-		return false;
-	},
-		
-	removeSimilarIssue: function (issue) {
-		if (this.hasSimilarIssue(issue)) {
-			return this.issues.splice(this.getSimilarIssueIndex(issue), 1);
-		}	
-	},
-		
-	hasSimilarIssuesAs: function (adFeedback) {
-		if (this.issues.length !== adFeedback.issues.length) {
-			return false;
-		}
+        return false;
+    },
 
-		return this.issues.every(function (issue) {
-			return adFeedback.hasSimilarIssue(issue);
-		});
-	}
+    insertIssue: function (issue) {
+        if (!this.hasIssue(issue)) {
+            if (this.hasSimilarIssue(issue)) {
+                this.getSimilarIssue(issue).combine(issue);
+            }
+            else {
+                this.issues.push(issue);
+            }
+        }
+    },
+
+    getIssueIndex: function (issue) {
+        for (var i = 0; i < this.issues.length; i++) {
+            if (this.issues[i].isEqualTo(issue)) {
+                return i;
+            }
+        }
+        return -1;
+    },
+
+    hasIssue: function (issue) {
+        if (this.getIssueIndex(issue) >= 0) {
+            return true;
+        }
+
+        return false;
+    },
+
+    getSimilarIssueIndex: function (issue) {
+        for (var i = 0; i < this.issues.length; i++) {
+            if (this.issues[i].isSimilarTo(issue)) {
+                return i;
+            }
+        }
+        return -1;
+    },
+
+    getSimilarIssue: function (issue) {
+        var i = this.getSimilarIssueIndex(issue);
+
+        if (i >= 0) {
+            return this.issues[i];
+        }
+
+        return undefined;
+    },
+
+    hasSimilarIssue: function (issue) {
+        if (this.getSimilarIssueIndex(issue) >= 0) {
+            return true;
+        }
+
+        return false;
+    },
+
+    removeSimilarIssue: function (issue) {
+        if (this.hasSimilarIssue(issue)) {
+            return this.issues.splice(this.getSimilarIssueIndex(issue), 1);
+        }
+    },
+
+    hasSimilarIssuesAs: function (adFeedback) {
+        if (this.issues.length !== adFeedback.issues.length) {
+            return false;
+        }
+
+        return this.issues.every(function (issue) {
+            return adFeedback.hasSimilarIssue(issue);
+        });
+    }
 };
 
 function Ad(args) {
-	this.id = args.id;
-	this.title = args.title;
-	this.format = args.format;
+    this.id = args.id;
+    this.title = args.title;
+    this.format = args.format;
 }
 
 Ad.prototype = {
-	isEqualTo: function (ad) {
-		return (this.id === ad.id &&
+    isEqualTo: function (ad) {
+        return (this.id === ad.id &&
 			this.title === ad.title &&
 			this.format === ad.format);
-	}
+    }
 };
 
 function Issue(args) {
-	this.category = args.category;
+    this.category = args.category;
 
-	if (args.adElement) {
-		this.adElements = [args.adElement];
-	}
+    if (args.adElement) {
+        this.adElements = [args.adElement];
+    }
 
-	if (args.problemsIssues) {
-		this.problemsIssues = args.problemsIssues.replace(/^Other/, "");
-	}
+    if (args.problemsIssues) {
+        this.problemsIssues = args.problemsIssues.replace(/^Other/, "");
+    }
 
-	if (args.testPage) {
-		this.testPages = [args.testPage];
-	}
+    if (args.testPage) {
+        this.testPages = [args.testPage];
+    }
 
-	if (args.additionalNotes) {
-		this.additionalNotes = args.additionalNotes.replace(/\s$/, "");
-	}
+    if (args.additionalNotes) {
+        this.additionalNotes = args.additionalNotes.replace(/\s$/, "");
+    }
 }
 
 Issue.prototype = {
-	insertAdElements: function (adElements) {
-		if(adElements && adElements.length > 0) {
-			this.adElements.push(adElements);
-		}	
-	},
+    insertAdElements: function (adElements) {
+        if (adElements && adElements.length > 0) {
+            this.adElements.push(adElements);
+        }
+    },
 
-	insertTestPages: function (testPages) {
-		if(testPages && testPages.length > 0) {
-			this.testPages.push(testPages);
+    insertTestPages: function (testPages) {
+        if (testPages && testPages.length > 0) {
+            this.testPages.push(testPages);
 
-                        this.testPages.forEach(function (testPage, i, testPages) {
-                            if (testPages.indexOf(testPage) >= 0) {
-                                testPages.splice(i, 1);
-                            }
-                        });
-		}	
-	},
+            this.testPages.forEach(function (testPage, i, testPages) {
+                if (testPages.indexOf(testPage) >= 0) {
+                    testPages.splice(i, 1);
+                }
+            });
+        }
+    },
 
-	combine: function (issue) {
-		if (this.isSimilarTo(issue)) {
-			this.insertAdElements(issue.adElements);
-			this.insertTestPages(issue.testPages);
-		}
-	},
+    combine: function (issue) {
+        if (this.isSimilarTo(issue)) {
+            this.insertAdElements(issue.adElements);
+            this.insertTestPages(issue.testPages);
+        }
+    },
 
-	isEqualTo: function (issue) {
-		return (this.adElements === issue.adElements &&
+    isEqualTo: function (issue) {
+        return (this.adElements === issue.adElements &&
 			this.testPage === issue.testPage &&
 			this.isSimilarTo(issue));
-	},
+    },
 
-	isSimilarTo: function (issue) {
-		return (this.category === issue.category &&
+    isSimilarTo: function (issue) {
+        return (this.category === issue.category &&
 			this.problemsIssues === issue.problemsIssues &&
 			this.additionalNotes === issue.additionalNotes);
-	}
+    }
+};
+
+Array.prototype.addUnique = function (val) {
+    if (this.indexOf(val) < 0) {
+        this.push(val);
+    }
 };
